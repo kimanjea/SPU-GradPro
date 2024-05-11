d89700dc737e268c0e20d33e2fce8a36cc732094
adebayoo <adebayoo@spu.edu>
2024-05-10 22:42:14 -0700

    added the require minigame
    now if you need to teleport you need to finish the minigame and require the required progression to move forward. If door does not need to have a minigame done then uncheck the box that says reequiregame

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -1,34 +1,43 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class teleport : MonoBehaviour
 {
     public bool addProgress; // Flag to determine whether to add progress when teleporting
-    Progress Progress;
+    Progress progression;
     public SceneSwap sceneSwap; // Reference to the SceneSwap script
     public bool needSwap = false;
     public string sceneName; // Name of the scene to teleport to
+    public bool requiresgame = false;
+    public int reqprogress = 0;
+
 
     private void OnTriggerEnter(Collider other)
     {
+    
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
-            if (sceneSwap)
-            {
-                sceneSwap.SwapAndTeleport(sceneName);
+            if (requiresgame && (!progression || progression.progression <= reqprogress)){
+
+                Debug.Log("You need to complete the game to unlock this level");
+                return;
+
+            }else{
+
+                SceneManager.LoadScene(sceneName);
+                if (sceneSwap)
+                {
+                    sceneSwap.SwapAndTeleport(sceneName);
+                }
+                Debug.Log("New Scene Loaded: " + sceneName);
+
+                SceneManager.LoadScene(sceneName);
             }
-            Debug.Log("New Scene Loaded: " + sceneName);
-            addprogress();
-         
-            SceneManager.LoadScene(sceneName);
+
             
         }
     }
 
-    void addprogress()
-    {
-        Progress.progression += 25;
-    }
-
 }
+
