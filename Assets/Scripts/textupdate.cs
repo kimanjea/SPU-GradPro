c2a4ff06e66704a2b5254f4d851777b8a8308d5c
adebayoo <adebayoo@spu.edu>
2024-05-31 20:34:11 -0700

    made the change to the linked list room and also added the the other tablets along with the a place to show what nodes were chosen

--- Assets/Scripts/textupdate.cs
+++ Assets/Scripts/textupdate.cs
@@ -1,27 +1,24 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using UnityEditor;
 using System.Collections.Generic;
 
 public class TextUpdate : MonoBehaviour
 {
-    public TMP_Text uiText; // Reference to a UI Text component
-    public Linked_list linkedlist;
+    public TMP_Text uiText; 
+    public Linked_list linkedList; 
+    public bool isChoice = false;
 
-
-    
     void Update()
     {
-       if (linkedlist != null){
-        
-        uiText.text = updateNodeText();
-
+        if (linkedList != null && uiText != null)
+        {
+            uiText.text = isChoice ? UpdateChoiceNodeText() : UpdateNodeText();
         }
     }
-    string updateNodeText()
+
+    string UpdateNodeText()
     {
-        GameObject[] currentNodes = linkedlist.node;
+        GameObject[] currentNodes = linkedList.node;
 
         if (currentNodes != null && currentNodes.Length > 0)
         {
@@ -37,5 +34,25 @@ public class TextUpdate : MonoBehaviour
             return formattedText;
         }
         return "No nodes available";
+    }
+
+    string UpdateChoiceNodeText() // Shows the player's current choice to see what their list looks like
+    {
+        List<GameObject> currentNodes = linkedList.usernodes;
+
+        if (currentNodes != null && currentNodes.Count > 0)
+        {
+            string formattedText = "";
+            for (int i = 0; i < currentNodes.Count; i++)
+            {
+                formattedText += currentNodes[i].name;
+                if (i < currentNodes.Count - 1)
+                {
+                    formattedText += "->";
+                }
+            }
+            return formattedText;
+        }
+        return "No nodes selected";
     }
 }
